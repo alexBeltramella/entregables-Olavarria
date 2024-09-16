e17030044b5108cf833813846842f2b45e382588
Alex <beatsinka@gmail.com>
2024-09-16 18:16:56 -0300

    cambios en css de estilo

--- api/script.js
+++ api/script.js
@@ -6,15 +6,29 @@ fetch("https://fakestoreapi.com/products")
 //convertir a json
 .then(resp => resp.json())
 
-//me muestre por consola
+//me muestre los productos n index
 .then(data => {
     data.forEach(prod => {
         contenedor.innerHTML += 
-        `<img class="img-prod" src="${prod.image}" alt="productos">
-        <h2 class="titulo">${prod.title}</h2>
-        <p class="descripcion">Descripcion: ${prod.description}</p>
-        <h4 class="precio">Precio: ${prod.price}</h4>
-        <p class="rating">${prod.rating.count} - ${prod.rating.rate}</p>`
+        `<div class="productos">
+            <div class="img-container">
+                <img class="img-prod" src="${prod.image}" alt="productos">
+            </div>
+
+            <div class="titulo-container">
+                <h2 class="titulo">${prod.title}</h2>
+            </div>
+
+            <div class="descripcion-container">
+                <p class="descripcion">Descripcion: ${prod.description}</p>
+            </div>
+            
+            <div class="precio-container">
+                <h4 class="precio">Precio: ${prod.price}</h4>
+                <p class="rating">${prod.rating.count} - ${prod.rating.rate}</p>
+            </div>
+        </div>
+`
     })
 })
 
